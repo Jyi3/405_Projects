bfb8fc9ab79b7bcc38d0da539b1cfb72cc51a6ad
Jyi3 <jyi3@mail.umw.edu>
2023-03-10 23:52:20 -0500

    push
    created a powerfull kill command, working on fork with cat and grep

--- shellproject/shell.c
+++ shellproject/shell.c
@@ -5,29 +5,22 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <dirent.h>
+#include <fcntl.h>
 
 #define MAX_COMMAND_LENGTH 100
 #define MAX_ARGS 10
 
 volatile sig_atomic_t interrupted = 0;
-//volatile sig_atomic_t kill_interrupted = 0;
 
 void handle_interrupt(int signal) {
     interrupted = 1;
 }
-
-//void handle_kill(int signal) {
-//    kill_interrupted = 1;
-//}
 
 int main() {
     char command[MAX_COMMAND_LENGTH];
 
     // Set up the interrupt signal handler
     signal(SIGINT, handle_interrupt);
-
-    // Set up the interrupt signal handler
-    //signal(SIGKILL, handle_kill);
 
     DIR *dir;
     struct dirent *ent;
@@ -39,22 +32,17 @@ int main() {
             continue;
         }
 
-//        if (kill_interrupted) {
-//            kill_interrupted = 0;
-//            printf("\n");
-//            break;
-//        }
-
         // Display the prompt and get user input
         printf("gshell> ");
         if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
             // End of input stream (Ctrl+D)
             printf("\n");
             return 0;
-            break;
         }
+
         //remove trailing new line
         command[strcspn(command, "\n")] = '\0'; 
+
         char *token;
         char *args[MAX_ARGS];
         int i = 0;
@@ -65,8 +53,7 @@ int main() {
             token = strtok(NULL, " ");
             i++;
         }
-        //for (int j = 0; j < i; j++){
-          // printf("%s\n", args[j]);}
+
         if (i == 0 || args[0] == NULL) {
             continue;
         }else if (strcmp(args[0], "cd") == 0){
@@ -78,22 +65,86 @@ int main() {
         }else if (strcmp(args[0], "cls") == 0) {
             dir = opendir("."); 
             while ((ent = readdir(dir)) != NULL){
-                    printf("%s\n", ent->d_name);
+                printf("%s\n", ent->d_name);
             }
-                closedir(dir);
-                
+            closedir(dir);
         }else {
             int pid = fork();
             if (pid == 0){
-                execvp(args[0], args);
+                
+                if (command != NULL && args[0] != NULL)
+                {
+                    execvp(args[0], args);
+                    // Execute the command
+                    system(command);
+                        
+                    int p = 0;
+                    while (args[p] != NULL)
+                    {
+                        printf("%s is in args pos: %d \n",args[p], p);
+                        args[p] = NULL;
+                        p++;
+                    }
+
+                }
+                else if(command != NULL && args[0] != NULL)
+                {
+                    // Check for input redirection
+                    for (int j = 0; j < i; j++) 
+                    {
+                        if (strcmp(args[j], "<") == 0) 
+                        {
+                            int input_fd = open(args[j+1], O_RDONLY);
+                            dup2(input_fd, STDIN_FILENO);
+                            close(input_fd);
+                            args[j] = NULL;
+                            printf("%s is in args pos: %d \n",args[j], j);
+                        }
+                    }
+
+                    // Check for output redirection
+                    for (int j = 0; j < i; j++) 
+                    {
+                        if (strcmp(args[j], ">") == 0) 
+                        {
+                            int output_fd = open(args[j+1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+                            dup2(output_fd, STDOUT_FILENO);
+                            close(output_fd);
+                            printf("%s is in args pos: %d \n",args[j], j);
+                            args[j] = NULL;
+                        }
+                    }
+
+                    
+                    int p = 0;
+                    while (args[p] != NULL)
+                    {
+                        printf("%s is in args pos: %d \n",args[p], p);
+                        args[p] = NULL;
+                        p++;
+                    }
+
+                    
+                }
+                else
+                {
+                    int p = 0;
+                    while (args[p] != NULL)
+                    {
+                        printf("%s is in args pos: %d \n",args[p], p);
+                        args[p] = NULL;
+                        p++;
+                    }
+                    printf("Unknown command\n");
+                }
+                exit(0);
             }else if (pid > 0){
                 int status;
                 waitpid(pid, &status, 0);
             }
-            
         }
-        // Execute the command
-        system(command);
+        
+
     }
 
     return 0;
